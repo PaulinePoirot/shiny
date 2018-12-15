1ae949c635a54ed46e94fd917f79d83293cd3aa7
Pauline Poirot <rivaille.cass@gmail.com>
2018-12-15 11:35:00 +0100

    Merge branch 'feature/animation'

--- app/src/main/java/android/shiny/view/main_activity/PokemonHolder.java
+++ app/src/main/java/android/shiny/view/main_activity/PokemonHolder.java
@@ -1,5 +1,6 @@
 package android.shiny.view.main_activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.shiny.R;
 import android.shiny.view.pokemon_activity.PokemonActivity;
@@ -8,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.view.ViewCompat;
 
 public class PokemonHolder extends RecyclerView.ViewHolder {
     public CardView card;
@@ -25,7 +28,9 @@ public class PokemonHolder extends RecyclerView.ViewHolder {
             public void onClick(View view) {
                 Intent intent = new Intent(itemView.getContext(), PokemonActivity.class);
                 intent.putExtra("id", getAdapterPosition()+1);
-                itemView.getContext().startActivity(intent);
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) itemView.getContext(), artwork, ViewCompat.getTransitionName(artwork));
+                itemView.getContext().startActivity(intent, options.toBundle());
+
             }
         });
     }
